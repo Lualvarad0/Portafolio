984f14ba92f5ddec3663a19ed2504aafe429ca95
MennovaConsulting <200856835+MennovaConsulting@users.noreply.github.com>
2026-04-27 19:55:23 -0500

    chore: remove Frontend/ folder

--- Frontend/src/interfaces/IMenubar.tsx
+++
@@ -1,4 +0,0 @@
-export interface MenubarProps {
-  time: string;
-  date: string;
-}
--- Frontend/src/interfaces/Iwindow.tsx
+++
@@ -1,8 +0,0 @@
-export interface IWindow {
-  title: string;
-  isActive: boolean;
-  onClose: () => void;
-  onMinimize: () => void;
-  position?: { top?: number; left?: number };
-  children: React.ReactNode;
-}
